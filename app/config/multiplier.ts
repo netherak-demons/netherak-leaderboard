3bf2641278e05ff33d4ff41818b8a7b7eb564bd9
pedrofalco <fpfalco@gmail.com>
2026-03-10 21:21:23 +0100

    fix multiplier

--- app/config/multiplier.ts
+++ app/config/multiplier.ts
@@ -1,13 +1,13 @@
 /**
  * Multiplier configuration
- * - Imuran Book: x3
- * - PFP NFT: x2
+ * - Imuran Book: x2 (+1)
+ * - PFP NFT: x1.5 (+0.5)
  * - None: x1
- * - Both stack: x3 + x2 = x5
+ * - Both stack: x2.5
  */
 export function getMultiplier(hasImuranBook: boolean, hasPfp: boolean): number {
   let multiplier = 1
-  if (hasImuranBook) multiplier += 2
-  if (hasPfp) multiplier += 1.5
+  if (hasImuranBook) multiplier += 1
+  if (hasPfp) multiplier += 0.5
   return multiplier
 }
